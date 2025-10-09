742994ecde451bd06657a488cd9b97fe9a11dfc0
Cristian Mitchell Harders López <cristianharders@gmail.com>
2025-10-09 16:41:00 +0200

    completed dataset controller

--- src/main/java/com/sheetvision/sheetvision/api/controller/DatasetController.java
+++ src/main/java/com/sheetvision/sheetvision/api/controller/DatasetController.java
@@ -1,11 +1,14 @@
 package com.sheetvision.sheetvision.api.controller;
 
+import com.sheetvision.sheetvision.api.model.DatasetCreateDTO;
 import com.sheetvision.sheetvision.api.model.DatasetDTO;
 import com.sheetvision.sheetvision.api.service.DatasetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,6 +30,30 @@ public class DatasetController {
     @GetMapping("/{id}")
     public ResponseEntity<DatasetDTO> getById(@PathVariable Long id){
         return ResponseEntity.ok(datasetService.getDatasetById(id));
+    }
+
+    //create dataset
+    @PostMapping
+    public ResponseEntity<DatasetDTO> createDataset(@RequestBody DatasetCreateDTO dto){
+
+        DatasetDTO created = datasetService.createDataset(dto);
+
+        URI location = URI.create("/api/datasets/" + created.id());
+
+        return ResponseEntity.created(location).body(created);
+    }
+
+    //update dataset
+    @PutMapping("/{id}")
+    public ResponseEntity<DatasetDTO> updateById(@PathVariable Long id, @RequestBody DatasetCreateDTO dto){
+        return ResponseEntity.ok(datasetService.updateDataset(id, dto));
+    }
+
+    //delete dataset
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteDataset(@PathVariable Long id){
+        datasetService.deleteDataset(id);
     }
 
 }
